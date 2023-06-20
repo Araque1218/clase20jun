c67f1ea8b474fdbc07ddb74f57f9a3110e8563b5
Mateo_Araque34 <131192048+Araque1218@users.noreply.github.com>
2023-06-20 13:12:08 -0500

    Primera parte

---
+++ ejercicio.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+int main(int argc, char const *argv[])
+{
+    struct persona
+    {
+        int matricula;
+        char nombre [20];
+        char direccion [50];
+        char carrera [25];
+        float promedio;
+    };
+
+ 
+   return 0;  
+   
+}
+
+
